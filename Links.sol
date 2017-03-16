1fe7d764b3de618b7871c78a51f641240eb38c27
ysnikhil <nikhil.nyk007@gmail.com>
2017-03-16 09:08:42 +0530

    Update Links.sol

--- Links.sol
+++ Links.sol
@@ -1,26 +1,68 @@
 pragma solidity ^0.4.4;
 
-contract Links{
+contract ExpiringLinks{
 
-	uint public deadline;
+	struct user{
 
-	function Links();
-	
-	//To make the contract disable after a specific period of time
-	function startTimer (uint _deadline){
+		address userAddress;
+		uint256 amount;
+	}
 
-		deadline = block.number  + _deadline;
+
+
+	struct Link {
+
+		address providerAddress;
+		string  link;
+		bool linkStatus;
+		uint linkAccessCount;
 
 	}
 
-	function timeUp() returns (bool done) {
 
-		if(deadline <= block.number) {
-			return true;
-		}
-	
-		return false;
+
+	mapping(string => Link) public Links;
+	uint public linkAccessed;
+
+
+	function ExpiringLinks();
+
+
+
+	function newLink(address _providerAddress, uint _link, string _songname)  {
+
+		Link L = Links[_songname];
+		L.providerAddress = _providerAddress;
 	}
 
+
+
+    
+    function buySongLink(string _songname, uint256 _amount) returns (string link) {
+
+    	Link L = Links[_songname];
+    	user memory u;
+    	u.userAddress = msg.sender;
+    	u.amount = _amount;
+
+    	if(L.linkAccessCount <= 2){
+
+    		L.link="Coldplay/clocks.mp3";
+
+    	}
+
+    	else {
+
+    		throw;
+    	}
+
+    	L.linkAccessCount = linkAccessed++;
+
+
+
+
+
+
+    }
 
 }
